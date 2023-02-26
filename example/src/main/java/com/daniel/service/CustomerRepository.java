ccebd2413c285907a934ca4966c8f9adfbbe3956
Daniel <88918586+dzniel@users.noreply.github.com>
2023-02-26 20:59:07 +0700

    readjust spacing

--- example/src/main/java/com/daniel/service/CustomerRepository.java
+++ example/src/main/java/com/daniel/service/CustomerRepository.java
@@ -5,8 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
 @Service
-public interface CustomerRepository extends JpaRepository<Customer, Integer> {
-
-    //
-
-}
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {}
